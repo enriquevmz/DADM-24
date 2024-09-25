1cc47be959038ecd0b4dc7047fb2ca82807b48f2
enriquevmz <evelazquezm11@gmail.com>
2024-09-25 18:47:35 +0000

    ⭐FEAT:App Vue Add radio buttonn

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,38 @@
 import {ref} from "vue";
 //modelo 
 const header = ref('App lista de compras');
+const items = ref([
+    {id:'0', label: '10 bolillos'},
+    {id:'1', label:'1 lata de frijoles'},
+    {id:'2', label:'Six de chelas'},
+    {id:'3', label:'1 Nuetella'},
+
+]);
+const newItem =ref('');
+const newItemPriority= ref('low');
 </script>
 
 <template>
-<h1> 
+ <h1> 
     <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }} 
-    </h1>
+ </h1>
+ <input v-model="newItem"
+type="text" 
+placeholder="Agregar articulo">
+<!--Radio Buttons-->
+<label>
+    <input type="radio" value="low" v-model="newItemPriority">
+    Bajo
+</label>
+<label>
+    <input type="radio" value="high" v-model="newItemPriority">
+    Alto
+</label>
+{{ newItemPriority =='low'?'🦊':'🐻'}}
+ <ul>
+    <li v-for="item in items" :key="item.id"> 🛒 {{  item.label }}</li>
+ </ul>
 </template>
 
 <style scoped>
