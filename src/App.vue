3bb85a8c6125f050d58c942b2d9816e18c6d5eb2
enriquevmz <evelazquezm11@gmail.com>
2024-09-30 17:54:52 +0000

    🌟FEAT:  enlace radion button

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,50 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 //modelo 
-const header = ref('App lista de compras');
+const header = ref('App lista de Compras');
 const items = ref([
-    {id:'0', label: '10 bolillos'},
-    {id:'1', label:'1 lata de frijoles'},
-    {id:'2', label:'Six de chelas'},
-    {id:'3', label:'1 Nuetella'},
-
+    {id:'0', label: '10 bolillos'}, 
+    {id:'1', label: '1 lata de frijol'}, 
+    {id:'2', label: '50 g de queso'},
+    {id:'3', label: '10 coca cola ligth'}
 ]);
-const newItem =ref('');
-const newItemPriority= ref('low');
+const newItem = ref(''); 
+const newItemHighPriority = ref(false);
 </script>
 
 <template>
- <h1> 
+<h1>
     <i class="material-icons shopping-cart-icon">local_mall</i>
-    {{ header }} 
- </h1>
- <input v-model="newItem"
-type="text" 
-placeholder="Agregar articulo">
-<!--Radio Buttons-->
-<label>
-    <input type="radio" value="low" v-model="newItemPriority">
-    Bajo
-</label>
-<label>
-    <input type="radio" value="high" v-model="newItemPriority">
-    Alto
-</label>
-{{ newItemPriority =='low'?'🦊':'🐻'}}
- <ul>
-    <li v-for="item in items" :key="item.id"> 🛒 {{  item.label }}</li>
- </ul>
+    {{ header }}
+</h1>
+
+<!-- Agrupando en un div las entradas -->
+<form class="add-item fomr" v-on:submit.prevent="items.push({ id: items.length + 1, label: newItem })">
+    
+    <!-- entrada de texto -->
+    <input type="text" 
+    placeholder="Agregar"  
+    v-model.trim="newItem">
+    
+    <!-- Caja de seleccion de prioridad -->
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" />
+      Super Importante 
+    </label>
+
+    <!-- Boton -->
+    <button class="btn btn-primary">
+    Guardar</button>
+  
+</form>
+    
+    <ul>
+        <li v-for="({id,label}, i) in items" :key="id"> {{ i+1 }} {{i%2==0?'🌟':'🛒'}} {{label}} </li>
+    </ul>
 </template>
 
 <style scoped>
-.shopping-cart-icon {
-    font-size: 3rem;
+.shopping-cart-icon{
+    font-size: 2rem;
 }
 </style>
