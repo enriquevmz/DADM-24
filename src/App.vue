656497bb8ed43707f18b1978565094e171b47fca
enriquevmz <evelazquezm11@gmail.com>
2024-11-06 18:45:15 +0000

    🌟S03S03 Propiertes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
+import { ref } from 'vue';
 import planItem from './components/plan-item.vue';
+const planes = 
+ref(['El cafetero', 'El chelas', 'El genio']);
 </script>
 
 <template>
@@ -19,9 +22,7 @@ import planItem from './components/plan-item.vue';
     </h2>
 
     <div class="plans">
-      <plan-item></plan-item>
-      <plan-item></plan-item>
-      <plan-item></plan-item>
+      <plan-item v-for="plan in planes" :key="plan" :name="plan"/>
       </div>
   </div>
 </template>
--- src/components/plan-item.vue
+++ src/components/plan-item.vue
@@ -2,15 +2,23 @@
     <div class="plan">
       <div class="description">
         <span class="title">
-          El adicto
+       {{ name }} 
         </span>
       </div>
     </div>
   </template>
   
   <script setup>
-  
-  </script>
+  //Usando un macro para
+  //deginir las props
+  //defineProps(['name','planIcon']);
+  defineProps({
+    name: {
+      type: String,
+      required: true
+    }
+  });
+ </script>
   
   <style scoped>
   
