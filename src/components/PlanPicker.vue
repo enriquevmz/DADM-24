015f7e9c84468f16b149b132dba34d4f92d59ca6
enriquevmz <evelazquezm11@gmail.com>
2024-11-08 17:00:23 +0000

    🌟Componentes Anidados

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,13 +1,13 @@
 <script setup>
         
-        import PlanItem from './PlanItem.vue';
+        import PlanPickerItem from './PlanPickerItem.vue';
         import {ref} from 'vue';
 
 const plans = ref(['El soltero', 'El curioso','El adicto']);
         </script>
 <template>
     <div class="plans">
-      <PlanItem v-for="plan in plans" :key="plan" :name="plan"></PlanItem>
+      <PlanPickerItem v-for="plan in plans" :key="plan" :name="plan"/>
     </div>
 </template>
 
