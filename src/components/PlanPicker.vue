e26252206a95115f649d11b3d5cfd01eee89ac1c
enriquevmz <evelazquezm11@gmail.com>
2024-11-08 16:53:38 +0000

    🌟Adiccional de la act S03S04

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,13 +1,13 @@
 <script setup>
         
-        import planItem from './plan-item.vue';
+        import PlanItem from './PlanItem.vue';
         import {ref} from 'vue';
 
 const plans = ref(['El soltero', 'El curioso','El adicto']);
         </script>
 <template>
     <div class="plans">
-      <plan-item v-for="plan in plans" :key="plan" :name="plan"></plan-item>
+      <PlanItem v-for="plan in plans" :key="plan" :name="plan"></PlanItem>
     </div>
 </template>
 
