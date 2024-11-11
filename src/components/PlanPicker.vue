98591ae720dac1dc59a11949b95aa82aada93169
enriquevmz <evelazquezm11@gmail.com>
2024-11-11 18:51:27 +0000

    🚧S0305 EVENTOS Add events to child component

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -4,10 +4,16 @@
         import {ref} from 'vue';
 
 const plans = ref(['El soltero', 'El curioso','El adicto']);
+const selectPlan = (name) => {
+        console.log(name);
+}
         </script>
 <template>
     <div class="plans">
-      <PlanPickerItem v-for="plan in plans" :key="plan" :name="plan"/>
+      <PlanPickerItem v-for="plan in plans" 
+      :key="plan" 
+      :name="plan"
+      @select="selectPlan"/>
     </div>
 </template>
 
--- src/components/PlanPickerItem.vue
+++ src/components/PlanPickerItem.vue
@@ -1,23 +1,37 @@
 <template>
-    <div class="plan">
+    <div @click="selecPlan" class="plan">
       <div class="description">
         <span class="title">
-       {{ name }} 
+       {{ name }}  {{ selected ? '🦖' : '' }}
         </span>
       </div>
     </div>
   </template>
   
   <script setup>
+  //impoetando ref funtion
+  import {ref} from 'vue';
   //Usando un macro para
   //deginir las props
   //defineProps(['name','planIcon']);
-  defineProps({
+  const props = defineProps({
     name: {
       type: String,
       required: true
     }
   });
+
+  //definiedo event
+   const emit = defineEmits(['select'])
+  //variable que permite recordar si el plan ha sido seleccionado 
+  const selected = ref(false);
+  //funcion que permite cambiar el estado de la variable selected
+  const selecPlan = () => {
+    selected.value = ! selected.value;
+  };
+  //emitir evento de que ha sido selccionado 
+  emit('select', props.name);
+
  </script>
   
   <style scoped>
